0063cf9ce199593d52e5cec28cee3b00749f21bd
Hasnain-Siddiqui <71247479+Hasnain-Siddiqui@users.noreply.github.com>
2023-09-28 00:17:29 +0000

    data types

---
+++ 01_basics/variables.js
@@ -0,0 +1,7 @@
+const accountId=112
+let accountEmail = "mohdhasnain932@outlook.com"
+let accountPassword ="1234"
+accountCity ="Jaipur"
+let accountState
+
+console.table([accountId , accountEmail , accountCity , accountState])
